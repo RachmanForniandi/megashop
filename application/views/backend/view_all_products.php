dfb2a0f27d725a9f0757a2626a72af90fc20bc5b
RachmanForniandi <rachmanforniandi@gmail.com>
2017-08-04 22:17:59 +0700

    mockup dengan bootstrap & DataTables

--- application/views/backend/view_all_products.php
+++ application/views/backend/view_all_products.php
@@ -2,10 +2,24 @@
 <html>
 	<head>
 		<title>Admin Page | View All Products</title>
+		<!--Load JQuery dr CDN -->
+			<script type="text/javascript" language="javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+			<script type="text/javascript" language="javascript" src="//cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
+			<script type="text/javascript" language="javascript" src="//cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.js"></script>
+		<!--Load Bootstrap, & Datatables dr CDN -->	
+			<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+			<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.css">
+<!-- 			<style>
+				.row div{ border:#000 0px solid }
+			</style> -->
 	</head>
 	<body>
+		<!--Ket: dlm div row hrs ada col yg jumlah nilainya 12-->
+		<div class="row">
+			<div class="col-md-1"></div>
+			<div class="col-md-10">
 		<h1>Products Table</h1>
-		<table>
+		<table id="myTable" class="table table-striped table-bordered table-hover">
 			<thead>
 				<tr>
 					<th>#</th>
@@ -13,6 +27,12 @@
 					<th>Description</th>
 					<th>Price</th>
 					<th>Stock</th>
+					<th>
+						<?=anchor('admin/products/create',
+								  'Add Product', 
+								  ['class'=>'btn btn-primary btn-sm'])
+						?>
+					</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -23,9 +43,27 @@
 						<td><?=$product->description?></td>
 						<td><?=$product->price?></td>
 						<td><?=$product->stock?></td>
+						<td>
+							<?=anchor('admin/product/update/' . $product->id,
+									  'Edit',
+									  ['class'=>'btn btn-default btn-sm'])
+							?>
+							<?=anchor('admin/product/delete/' . $product->id,
+									  'Delete',
+									  ['class'=>'btn btn-danger btn-sm'])
+							?>
+						</td>
 					</tr>
 				<?php endforeach; ?>
 			</tbody>
 		</table>
+			</div>
+			<div class="col-md-1"></div>
+		</div>
+			<script>
+				$(document).ready(function(){
+				    $('#myTable').DataTable();
+				});
+			</script>
 	</body>
 </html>
