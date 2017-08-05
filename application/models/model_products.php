5da02c2526025b1d0b83333f1f67a646f30c1502
RachmanForniandi <rachmanforniandi@gmail.com>
2017-08-06 01:07:47 +0700

    fungsi edit gagal form edit blm muncul

--- application/models/model_products.php
+++ application/models/model_products.php
@@ -14,4 +14,34 @@ class model_products extends CI_Model
 		}
 	}
 
+	public function find($id){
+		//Query mencari record berdasarkan ID
+		$hasil = $this->db->where('id',$id)
+						  ->limit(1)
+						  ->get('products');
+		if ($hasil->num_rows()>0) {
+			return $hasil->row();
+		}else{
+			return array();
+		}
+	}
+
+	public function create($data_products){
+		//Query INSERT INTO...
+		$this->db->insert('products', $data_products);
+
+	}
+
+	public function upate($id, $data_products){
+		//Query UPDATE FROM...WHERE id=...
+		$this->db->where('id', $id)
+				 ->update('products',$data_products);
+	}
+
+	public function delete($id){
+		//Query DELETE FROM...WHERE id=...
+		$this->db->where('id',$id)
+				 ->delete('products');
+	}
+
 }
